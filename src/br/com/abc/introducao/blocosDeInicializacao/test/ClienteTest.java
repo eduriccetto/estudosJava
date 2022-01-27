2fbd38dee184247262a57228690c71ad97393ecc
Eduardo <eduardopr@ciandt.com>
2022-01-27 16:45:37 -0300

    [estudosJava]-Blocos de inicializacao

--- src/br/com/abc/introducao/blocosDeInicializacao/test/ClienteTest.java
+++ src/br/com/abc/introducao/blocosDeInicializacao/test/ClienteTest.java
@@ -1,4 +1,16 @@
 package br.com.abc.introducao.blocosDeInicializacao.test;
 
+import br.com.abc.introducao.blocosDeInicializacao.classes.Cliente;
+
 public class ClienteTest {
+    public static void main(String[] args) {
+
+        Cliente c = new Cliente();
+        System.out.println(" ");
+        System.out.println("Exibindo quantidade de parcelas possiveis");
+        for (int parcela: c.getParcelas()){
+            System.out.print(parcela + " ");
+        }
+
+    }
 }
