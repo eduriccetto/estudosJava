9df22686607346efdd175c197e63b179dc0ba544
Eduardo <eduardopr@ciandt.com>
2022-03-15 14:21:42 -0300

    [estudosJava]-[MJVNJ]- Inicio de curso

---
+++ src/br/com/abc/introducao/MJVNJ/Tipos_Primitivos/Aula02_TiposPrimitivos.java
@@ -0,0 +1,14 @@
+package br.com.abc.introducao.MJVNJ.Tipos_Primitivos;
+
+public class Aula02_TiposPrimitivos {
+    public static void main(String[] args) {
+
+        //     Tipos primitivos em Java são 8
+        // int, double, float, char, byte, short, long, boolean
+
+        int idade = 10;
+        System.out.println("A idade é "+idade+" anos!");
+
+    }
+
+}
