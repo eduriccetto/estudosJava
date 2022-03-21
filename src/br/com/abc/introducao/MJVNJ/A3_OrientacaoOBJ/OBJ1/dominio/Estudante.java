4db1868ff270e6df107d338d4ea6b069cc98404b
Eduardo <eduardopr@ciandt.com>
2022-03-21 16:15:10 -0300

    [estudosJava]-[MJVNJ]- Orientacao obj 1

---
+++ src/br/com/abc/introducao/MJVNJ/A3_OrientacaoOBJ/OBJ1/dominio/Estudante.java
@@ -0,0 +1,9 @@
+package br.com.abc.introducao.MJVNJ.A3_OrientacaoOBJ.OBJ1.dominio;
+
+public class Estudante {
+
+    public String nome;
+    public int idade;
+    public char sexo;
+
+}
---
+++ src/br/com/abc/introducao/MJVNJ/A3_OrientacaoOBJ/OBJ1/teste/EstudanteTeste.java
@@ -0,0 +1,18 @@
+package br.com.abc.introducao.MJVNJ.A3_OrientacaoOBJ.OBJ1.teste;
+
+import br.com.abc.introducao.MJVNJ.A3_OrientacaoOBJ.OBJ1.dominio.Estudante;
+
+public class EstudanteTeste {
+    public static void main(String[] args) {
+
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+
+    }
+}
